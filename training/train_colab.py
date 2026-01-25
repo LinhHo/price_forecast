c13705d641901c3d00958101d00deaf46502412d
Linh Ho <45103089+LinhHo@users.noreply.github.com>
2026-01-25 12:57:24 +0100

    Clean unfinished run_dir

--- training/train_colab.py
+++ training/train_colab.py
@@ -1,31 +1,37 @@
 from forecasting.model.tft_model import TFTPriceModel
+import shutil
 from infra.s3 import upload_file
 from config import setup_logging
 
 setup_logging()
 
-
 def train_and_upload(zone: str):
     model = TFTPriceModel(zone)
 
-    model.train(
-        start="2023-01-01",
-        end="2024-01-01",
-        max_epochs=5,  # 30,
-        batch_size=64,
-    )
+    try:
+        model.train(
+            start="2023-01-01",
+            end="2024-01-01",
+            max_epochs=5,  # 30,
+            batch_size=64,
+        )
+    
+        run_dir = model.run_dir
 
-    # artifacts
-    run_dir = model.run_dir
+        artifacts = [
+            ("model/tft.ckpt", run_dir / "model" / "tft.ckpt"),
+            ("training_dataset.pt", run_dir / "data" / "training_dataset.pt"),
+            ("meta.json", run_dir / "meta.json"),
+        ]
 
-    artifacts = [
-        ("model/tft.ckpt", run_dir / "model" / "tft.ckpt"),
-        ("training_dataset.pt", run_dir / "data" / "training_dataset.pt"),
-        ("meta.json", run_dir / "meta.json"),
-    ]
+        for key, path in artifacts:
+            s3_key = f"{zone}/runs/{model.run_id}/{key}"
+            upload_file(local_path=path, s3_key=s3_key)
 
-    for key, path in artifacts:
-        s3_key = f"{zone}/runs/{model.run_id}/{key}"
-        upload_file(local_path=path, s3_key=s3_key)
+        print(f"Uploaded run {model.run_id} to S3")
 
-    print(f"Uploaded run {model.run_id} to S3")
+    except Exception as e:
+        print("Training failed, cleaning up run_dir:", run_dir)
+        if run_dir.exists():
+            shutil.rmtree(run_dir)
+        raise e
