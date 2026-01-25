807f4183283415d607ba03b80ed2c87da83c630f
Linh Ho <45103089+LinhHo@users.noreply.github.com>
2026-01-25 13:05:18 +0100

    Fix bug, not remove for now

--- training/train_colab.py
+++ training/train_colab.py
@@ -1,39 +1,69 @@
 from forecasting.model.tft_model import TFTPriceModel
-import shutil
 from infra.s3 import upload_file
-from config import setup_logging
-
-setup_logging()
 
 
 def train_and_upload(zone: str):
     model = TFTPriceModel(zone)
+
+    model.train(
+        start="2023-01-01",
+        end="2024-01-01",
+        max_epochs=5,  # 30,
+        batch_size=64,
+    )
+
+    # artifacts
     run_dir = model.run_dir
 
-    try:
-        model.train(
-            start="2023-01-01",
-            end="2024-01-01",
-            max_epochs=5,  # 30,
-            batch_size=64,
-        )
+    artifacts = [
+        ("model/tft.ckpt", run_dir / "model" / "tft.ckpt"),
+        ("training_dataset.pt", run_dir / "data" / "training_dataset.pt"),
+        ("meta.json", run_dir / "meta.json"),
+    ]
 
-        # run_dir = model.run_dir
+    for key, path in artifacts:
+        s3_key = f"{zone}/runs/{model.run_id}/{key}"
+        upload_file(local_path=path, s3_key=s3_key)
 
-        artifacts = [
-            ("model/tft.ckpt", run_dir / "model" / "tft.ckpt"),
-            ("training_dataset.pt", run_dir / "data" / "training_dataset.pt"),
-            ("meta.json", run_dir / "meta.json"),
-        ]
+    print(f"Uploaded run {model.run_id} to S3")
 
-        for key, path in artifacts:
-            s3_key = f"{zone}/runs/{model.run_id}/{key}"
-            upload_file(local_path=path, s3_key=s3_key)
 
-        print(f"Uploaded run {model.run_id} to S3")
+# from forecasting.model.tft_model import TFTPriceModel
+# from infra.s3 import upload_run
 
-    except Exception as e:
-        print("Training failed, cleaning up run_dir ", run_dir)
-        if run_dir.exists():
-            shutil.rmtree(run_dir)
-        raise e
+# def train_zone(zone: str):
+#     model = TFTPriceModel(zone)
+#     model.train(
+#         start="2023-01-01",
+#         end="2024-01-01",
+#         max_epochs=30,
+#         batch_size=64,
+#     )
+
+#     upload_run(zone, model.run_dir)
+
+
+# from forecasting.model.tft_model import TFTPriceModel
+# from infra.s3 import upload_file
+
+
+# def train_and_upload(zone: str):
+#     model = TFTPriceModel(zone)
+
+#     model.train(
+#         start="2023-01-01",
+#         end="2024-01-01",
+#         max_epochs=5,  # 30,
+#         batch_size=64,
+#     )
+
+#     # s3_prefix = f"{zone}/runs/{model.run_id}"
+#     # upload_file(model.run_dir, s3_prefix)
+
+#     s3_prefix = f"{zone}/runs/{model.run_id}/model/tft.ckpt"
+#     upload_file(
+#         local_path=model.run_dir / "model" / "tft.ckpt",
+#         s3_key=s3_prefix,
+#     )
+
+#     print(f"Uploaded to s3://price-forecast-tft-model/{s3_prefix}")
